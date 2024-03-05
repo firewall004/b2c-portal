450def49ea81235fc1b70cb98514a62b93e58a95
Vivek Kumar <kumar97vivek@gmail.com>
2024-03-05 18:37:37 +0530

    Add flash message to show success/failure

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -54,6 +54,25 @@
 
 
     <div class="flex-grow container mx-auto py-8">
+
+        @if (session('success'))
+            <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 mb-2 rounded relative"
+                role="alert">
+                <strong class="font-bold">Success!</strong>
+                <span class="block sm:inline">{{ session('success') }}</span>
+                <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                    <svg class="fill-current h-6 w-6 text-green-500" role="button" xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20">
+                        <title>Close</title>
+                        <path fill-rule="evenodd"
+                            d="M14.354 5.354a2 2 0 0 0-2.828 0L10 7.172 7.172 5.354a2 2 0 1 0-2.828 2.828L7.172 10l-2.828 2.828a2 2 0 1 0 2.828 2.828L10 12.828l2.828 2.828a2 2 0 1 0 2.828-2.828L12.828 10l2.828-2.828a2 2 0 0 0 0-2.828z"
+                            clip-rule="evenodd" />
+                    </svg>
+                </span>
+            </div>
+        @endif
+
+
         @yield('content')
     </div>
 
